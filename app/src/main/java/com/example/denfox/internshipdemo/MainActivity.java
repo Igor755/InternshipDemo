376217d78923c6c921936633f8b655440f44ca4b
denfox <denfox@denfoxs-MacBook-Pro.local>
2017-07-19 16:37:27 +0300

    Basic ListView w custom adapter implemented

--- app/src/main/java/com/example/denfox/internshipdemo/MainActivity.java
+++ app/src/main/java/com/example/denfox/internshipdemo/MainActivity.java
@@ -1,13 +1,64 @@
 package com.example.denfox.internshipdemo;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.example.denfox.internshipdemo.adapters.TaskAdapter;
+import com.example.denfox.internshipdemo.models.TaskItem;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView tasksListView;
+    private TaskAdapter adapter;
+    private ArrayList<TaskItem> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tasksListView = (ListView) findViewById(R.id.list_view);
+
+        items = new ArrayList<>();
+
+        items.add(new TaskItem(true, "Создать этот список", TaskItem.Type.ALARM, "10:00", "07/05/2017"));
+        items.add(new TaskItem(true, "Отметить первый пункт как готовый", TaskItem.Type.ALARM, "11:25", "07/05/2017"));
+        items.add(new TaskItem(true, "Осознать что 2 пункта уже готовы", TaskItem.Type.NOTE, "11:30", "07/05/2017"));
+        items.add(new TaskItem(false, "Вздремнуть", TaskItem.Type.PLACE, "11:35", "07/05/2017"));
+        items.add(new TaskItem(false, "Заказать воду", TaskItem.Type.ALARM, "16:00", "09/05/2017"));
+        items.add(new TaskItem(true, "Заплатить за интернет", TaskItem.Type.NOTE, "10:00", "11/05/2017"));
+        items.add(new TaskItem(true, "Придумать следующий элемент списка", TaskItem.Type.NOTE, "12:00", "11/05/2017"));
+        items.add(new TaskItem(false, "Сходить на работу", TaskItem.Type.PLACE, "09:00", "12/05/2017"));
+        items.add(new TaskItem(false, "Провести занятие интернатуры", TaskItem.Type.PLACE, "13:00", "12/05/2017"));
+        items.add(new TaskItem(false, "Развидеть увиденное", TaskItem.Type.ALARM, "00:00", "13/05/2017"));
+        items.add(new TaskItem(false, "Перевернуть пингвина", TaskItem.Type.NOTE, "22:00", "13/05/2017"));
+        items.add(new TaskItem(true, "Залипнуть в инете", TaskItem.Type.NOTE, "12:00", "14/05/2017"));
+        items.add(new TaskItem(true, "Впихнуть невпихуемое", TaskItem.Type.PLACE, "13:00", "15/05/2017"));
+
+
+        adapter = new TaskAdapter(items, this);
+
+        tasksListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Full task name")
+                        .setMessage(adapter.getItems().get(position).getTaskName())
+                        .setCancelable(true)
+                        .create().show();
+            }
+        });
+
+        tasksListView.setAdapter(adapter);
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/denfox/internshipdemo/adapters/TaskAdapter.java
@@ -0,0 +1,83 @@
+package com.example.denfox.internshipdemo.adapters;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.denfox.internshipdemo.R;
+import com.example.denfox.internshipdemo.models.TaskItem;
+
+import java.util.ArrayList;
+
+public class TaskAdapter extends BaseAdapter {
+
+    private ArrayList<TaskItem> items;
+    private Context ctx;
+
+    public TaskAdapter(ArrayList<TaskItem> items, Context ctx) {
+        this.items = items;
+        this.ctx = ctx;
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public TaskItem getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+
+        LayoutInflater inflater = LayoutInflater.from(ctx);
+
+        view = inflater.inflate(R.layout.main_list_item, viewGroup, false);
+
+        CheckBox isCompleted = view.findViewById(R.id.item_task_check);
+        TextView taskTitle = view.findViewById(R.id.item_task_title);
+        ImageView taskTypeImage = view.findViewById(R.id.item_task_type_icon);
+        TextView taskTime = view.findViewById(R.id.item_task_alarm_time);
+        TextView taskDate = view.findViewById(R.id.item_task_alarm_date);
+
+        isCompleted.setChecked(getItem(position).isCompleted());
+        taskTitle.setText(getItem(position).getTaskName());
+        taskDate.setText(getItem(position).getTaskDate());
+        taskTime.setText(getItem(position).getTaskTime());
+
+        switch (getItem(position).getTaskType()) {
+            case ALARM:
+                taskTypeImage.setImageDrawable(ContextCompat.getDrawable(ctx, R.drawable.ic_alarm_grey600_24dp));
+                break;
+            case NOTE:
+                taskTypeImage.setImageDrawable(ContextCompat.getDrawable(ctx, R.drawable.ic_note_outline_grey600_24dp));
+                break;
+            case PLACE:
+                taskTypeImage.setImageDrawable(ContextCompat.getDrawable(ctx, R.drawable.ic_map_marker_radius_grey600_24dp));
+                break;
+        }
+
+        return view;
+    }
+
+    public ArrayList<TaskItem> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<TaskItem> items) {
+        this.items = items;
+    }
+}
